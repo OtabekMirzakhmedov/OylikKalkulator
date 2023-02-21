f8abe9a65401c10f6c2dd4fea000ffd741bea0cd
Otabek Mirzakhmedov <Otabek.Mirzakhmedov@gmail.com>
2023-02-21 18:13:43 +0500

    some

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,65 @@
-import { Component } from '@angular/core';
+import { Component , OnInit} from '@angular/core';
+
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  
   title = 'OylikKalkulator';
+  daraja = 'option0';
+  oylik: number = 0;
+  univerReyting = 'top0';
+  univerReytingiUstamasi = 0;
+  value = 0;
+  ustama: number = 0;
+  summa: number = 0;
+  kpi: number = 0;
+  
+  updateUstama(event: any) { 
+    this.ustama = event.value;
+  }
+  
+  updateKpi(event: any) {
+    this.kpi = event.value;
+  }
+
+  
+  
+  ngOnInit(): void {
+    if (this.daraja == 'option0') {
+      this.oylik = 0;
+      
+    }
+    
+  }
+
+  Calculate() {
+    switch (this.daraja) {
+      case 'option0': this.oylik = 0; break;
+      case 'option1': this.oylik = 9000000; break;
+      case 'option2': this.oylik = 8000000; break;
+      case 'option3': this.oylik = 7000000; break;
+      case 'option4': this.oylik = 6000000; break;
+      case 'option5': this.oylik = 6000000; break;
+    }
+
+    switch (this.univerReyting) {
+      case 'top100': this.univerReytingiUstamasi = this.oylik; break;
+      case 'top300': this.univerReytingiUstamasi = this.oylik * 0.6; break;
+      case 'top500': this.univerReytingiUstamasi = this.oylik * 0.4; break;
+      case 'top1000': this.univerReytingiUstamasi = this.oylik * 0.1; break;
+      case 'top1000+': this.univerReytingiUstamasi = this.oylik * 0; break;
+      case 'top0': this.univerReytingiUstamasi = this.oylik * 0; break;
+    }
+
+    this.summa = this.oylik + this.univerReytingiUstamasi + this.oylik * this.ustama / 100 + this.oylik * this.kpi / 100;
+
+
+
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,29 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatSelectModule } from '@angular/material/select';
+import { MatButtonModule } from '@angular/material/button';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { MatSliderModule } from '@angular/material/slider';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatGridListModule,
+    MatDividerModule,
+    MatSelectModule,
+    MatButtonModule,
+    NgbModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
